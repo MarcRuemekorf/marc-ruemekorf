1b8d37bcef87a282524561ba154a74b7c1e8624c
Marc Ruemekorf <marc@doen.it>
2022-02-08 21:27:09 +0100

    Meta tags, favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,18 +1,18 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'cv-marc-ruemekorf',
+    title: 'Marc Ruemekorf',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'nl'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'CV van Marc Ruemekorf' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' }
     ]
   },
 
